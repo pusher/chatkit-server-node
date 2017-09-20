57fe7da45e2ed7898b7670d6e156d34ea0baf3ec
Hamilton Chapman <hamchapman@gmail.com>
2017-09-20 17:12:02 +0100

    Add two more examples

---
+++ examples/api_request.js
@@ -0,0 +1,32 @@
+var ChatKit = require('../target/index');
+
+// var PusherPlatform = require('../node_modules/pusher-platform-node/target/index');
+
+// Just for local testing where using self signed certs
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+// const baseClient = new PusherPlatform.BaseClient({
+//   host: 'localhost',
+//   port: 10443
+// })
+
+const chatkit = new ChatKit.default({
+  instanceId: 'v1:us1:example',
+  key: 'your:key'
+});
+
+chatkit.apiRequest({
+  method: 'POST',
+  path: '/rooms',
+  body: {
+    name: 'Hello from ham'
+  },
+  qs: {
+    test: 'hamagain'
+  },
+  jwt: chatkit.generateAccessToken({ userId: 'example' }).token
+}).then(() => {
+    console.log('Success');
+  }).catch((err) => {
+    console.log(err);
+  });
---
+++ examples/create_room.js
@@ -0,0 +1,23 @@
+var ChatKit = require('../target/index');
+
+// var PusherPlatform = require('../node_modules/pusher-platform-node/target/index');
+
+// Just for local testing where using self signed certs
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+// const baseClient = new PusherPlatform.BaseClient({
+//   host: 'localhost',
+//   port: 10443
+// })
+
+const chatkit = new ChatKit.default({
+  instanceId: 'v1:us1:example',
+  key: 'your:key'
+});
+
+chatkit.createRoom('example', 'my room')
+  .then(() => {
+    console.log('Success');
+  }).catch((err) => {
+    console.log(err);
+  });
