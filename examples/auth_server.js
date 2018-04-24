a1916ef32a18c89ee4db93f6ce24cfc8e82f4b0d
Hamilton Chapman <hamchapman@gmail.com>
2018-04-24 15:15:28 +0100

    Add auth_server example

---
+++ examples/auth_server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+var chatkitServer = require('../target/index');
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const chatkit = new chatkitServer.default({
+  instanceLocator: 'your:instance:locator',
+  key: 'your:key'
+});
+
+app.post('/auth', (req, res) => {
+  const authData = chatkit.authenticate({
+    userId: 'your-user-id'
+  });
+
+  res.status(authData.status)
+     .set(authData.headers)
+     .send(authData.body);
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
